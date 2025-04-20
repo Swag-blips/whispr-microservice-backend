2cf8ec706e2c33fada1f84df43dc3d106f6c726a
Swag-blips <coderblip@gmail.com>
2025-04-20 09:19:29 +0100

    feat:setup authservice with loggers and middlewares for efficient error handling

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import logger from "./utils/logger";
 import cors from "cors";
+import errorHandler from "./middleware/errorHandler";
+import logRequests from "./middleware/logRequests";
+import authRoutes from "./routes/auth.route";
 dotenv.config();
 
 const app = express();
@@ -9,9 +12,10 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3001;
 
-app.get("/api/auth/register", (req, res) => {
-  logger.info("Request received");
-});
+app.use(errorHandler);
+app.use(logRequests);
+
+app.get("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   logger.info(`auth service is running on port ${PORT}`);
