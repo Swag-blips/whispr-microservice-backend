449a0ce02d1610ba1ba8a31bb6933cf0e2e16200
Swag-blips <coderblip@gmail.com>
2025-04-24 17:41:08 +0100

    feat:mongodb search + caching result

--- user-service/src/controller/user.controller.ts
+++ user-service/src/controller/user.controller.ts
@@ -1,26 +1,41 @@
 import { Request, Response } from "express";
 import logger from "../utils/logger";
 import User from "../model/user.model";
-
+import redisClient from "../config/redis";
 export const getUser = async (req: Request, res: Response) => {
   logger.info("get user endpoint hit");
   try {
     const username = req.params.username;
-    logger.info(username);
-
+    const expiryTime = 5 * 60;
     if (!username) {
       res.status(400).json({ success: false, message: "Username required" });
       return;
     }
 
-    const user = await User.findOne({ username });
+    const cachedResult = await redisClient.get(`search:${username}`);
 
-    if (!user) {
-      res.status(404).json({ success: false, message: "User does not exist" });
+    if (cachedResult) {
+      res
+        .status(200)
+        .json({ success: true, cachedResult: JSON.parse(cachedResult) });
       return;
     }
 
-    res.status(200).json({ success: true, user: user });
+    const results = await User.find(
+      { $text: { $search: username } },
+      { score: { $meta: "textScore" } }
+    )
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10)
+      .lean();
+
+    res.status(200).json({ success: true, results });
+    await redisClient.set(
+      `search:${username}`,
+      JSON.stringify(results),
+      "EX",
+      expiryTime
+    );
     return;
   } catch (error) {
     logger.error("An error occured in the getUser controller", error);
--- user-service/src/model/user.model.ts
+++ user-service/src/model/user.model.ts
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 import { User } from "../types/types";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "Not much here yet... but trust me, I’m interesting.",
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  bio: {
-    type: String,
-    default: "Not much here yet... but trust me, I’m interesting.",
-  },
-  avatar: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.index({ username: "text" });
 const User = mongoose.model<User>("User", userSchema);
